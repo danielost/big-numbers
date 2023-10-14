ee77efd58e3d263ef4de7848adbe9c7b3880cad1
Daniel <danya.ostap@gmail.com>
2023-10-14 22:32:37 +0300

    Add `BigNumber` struct

---
+++ src/bignumbers.go
@@ -0,0 +1,29 @@
+package bignumbers
+
+type BigNumber struct {
+	blocks []Uint
+	hex    string
+}
+
+func (bn *BigNumber) SetHex(hex string) error {
+	stringBlocks := breakStringIntoBlocks(hex, 16)
+	uintBlocks := make([]Uint, 0)
+	for _, b := range stringBlocks {
+		var u Uint
+		if err := u.SetHex(b); err != nil {
+			return err
+		}
+		uintBlocks = append(uintBlocks, u)
+	}
+	bn.hex = hex
+	bn.blocks = uintBlocks
+
+	return nil
+}
+
+func (bn *BigNumber) GetHex() (hex string) {
+	for _, block := range bn.blocks {
+		hex = block.GetHex() + hex
+	}
+	return hex
+}
