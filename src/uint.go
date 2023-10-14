3420b17b9dbeb7c8caf503e259a7bd6199356267
Daniel <danya.ostap@gmail.com>
2023-10-14 18:12:08 +0300

    Convert `Uint` to a struct and add more getters and setters

--- src/uint.go
+++ src/uint.go
@@ -6,40 +6,74 @@ import (
 	"strings"
 )
 
-type Uint uint64
+type Uint struct {
+	value uint64
+}
 
 const hexDigits string = "0123456789abcdef"
 
-func (u Uint) ToHex() string {
-	var sb strings.Builder
-
-	for u > 0 {
-		sb.WriteByte(hexDigits[u%16])
-		u /= 16
+func (u *Uint) GetHex() (hex string) {
+	value := u.value
+	for value > 0 {
+		hex = string(hexDigits[value%16]) + hex
+		value /= 16
 	}
-
-	return sb.String()
+	return
 }
 
-func FromHex(hex string) Uint {
-	u := Uint(0)
-
+func (u *Uint) SetHex(hex string) error {
 	validatedHex, err := ValidateHex(hex)
 	if err != nil {
-		fmt.Println(err)
-		return 0
+		return err
 	}
 
+	value := *new(uint64)
 	for i, r := range validatedHex {
 		pow := math.Pow(16, float64(len(validatedHex)-i-1))
-		if r >= 0 && r <= 9 {
-			u += Uint(r-'0') * Uint(pow)
+		if r >= '0' && r <= '9' {
+			left := uint64(r - '0')
+			value += left * uint64(pow)
 		} else {
-			u += (Uint(r-'a') + 10) * Uint(pow)
+			left := uint64(uint64(r-'a') + 10)
+			value += left * uint64(pow)
 		}
 	}
+	u.value = value
 
-	return u
+	return nil
+}
+
+func (u *Uint) GetDecimal() uint64 {
+	return u.value
+}
+
+func (u *Uint) SetDecimal(value uint64) {
+	u.value = value
+}
+
+func (u *Uint) GetBinary() (bin string) {
+	value := u.value
+	for value > 0 {
+		bin = fmt.Sprint(value%2) + bin
+		value /= 2
+	}
+	return
+}
+
+func (u *Uint) SetBinary(bin string) error {
+	if err := ValidateBinary(bin); err != nil {
+		return err
+	}
+
+	value := *new(uint64)
+	for i, digit := range bin {
+		left := uint64(digit - '0')
+		right := uint64(math.Pow(2, float64(len(bin)-i-1)))
+		value += left * right
+	}
+	u.value = value
+
+	return nil
 }
 
 func ValidateHex(hex string) (string, error) {
@@ -53,4 +87,16 @@ func ValidateHex(hex string) (string, error) {
 		}
 	}
 	return hex, nil
+}
+
+func ValidateBinary(bin string) error {
+	if len(bin) > 64 {
+		return fmt.Errorf("max binary length is 64")
+	}
+	for _, digit := range bin {
+		if digit != '0' && digit != '1' {
+			return fmt.Errorf("binary string must contain only ones and zeros")
+		}
+	}
+	return nil
 }
